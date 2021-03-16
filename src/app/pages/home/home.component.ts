f0eefab0031add06ff051808e61da52743815a88
Daniel Cox <tdanielcox@gmail.com>
2021-03-15 21:30:18 -0600

    fixed numbering, one less bug on homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent implements OnInit {
       index: 2,
       title: 'Fix the Issues on the Homepage',
       linkTo: null,
-      content: 'There are several problems with this page, find and fix the issues.',
+      content: 'There are a few problems on the Homepage, find and fix the issues.',
     },
     {
       index: 3,
@@ -35,21 +35,21 @@ export class HomeComponent implements OnInit {
       content: 'Use the openweathermap.org api to show the user\'s latest temperature information in the header.',
     },
     {
-      index: 4,
+      index: 5,
       title: 'Complete the Taskboard Logic',
       linkTo: 'routerLink',
       link: '/taskboard',
       content: 'Navigate to the taskboard page and complete the logic as described.',
     },
     {
-      index: 5,
+      index: 6,
       title: 'Create a Notes Component',
       linkTo: 'routerLink',
       link: '/notes',
       content: 'We need to be able to capture notes about this site. Write a new component that will allow us to capture and view notes.',
     },
     {
-      index: 6,
+      index: 7,
       title: 'Commit Your Code After Each Task',
       linkTo: null,
       content: 'After each task is complete, commit the code to your repo using the task\'s title as the commit message.',
